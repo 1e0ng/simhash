36f9d179cadb5ae41cea643defe1e0da3d242c94
Liang Sun <i@liangsun.org>
2013-12-09 21:31:15 +0800

    Remove duplicated features.

--- tests/test_simhash.py
+++ tests/test_simhash.py
@@ -6,10 +6,10 @@ from simhash import Simhash
 class TestSegmentTree(TestCase):
     def test_segtree(self):
         sh = Simhash('How are you? I am fine. Thanks. And you?')
-        self.assertEqual(sh.value, 6460565663990245323)
+        self.assertEqual(sh.value, 6460601123240241099)
 
         sh2 = Simhash('How old are you ? :-) I am fine. Thanks. And you?')
-        self.assertEqual(sh.distance(sh2), 8)
+        self.assertEqual(sh.distance(sh2), 9)
 
         sh3 = Simhash(sh2)
         self.assertEqual(sh2.distance(sh3), 0)
