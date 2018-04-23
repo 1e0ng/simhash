098594700b0b8d782b599cb0d74eb0f7de770c7b
Leon <i@leons.im>
2018-04-23 23:06:17 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'simhash',
-    version = '1.8.0',
+    version = '1.9.0',
     keywords = ('simhash'),
     description = 'A Python implementation of Simhash Algorithm',
     license = 'MIT License',
