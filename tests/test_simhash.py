af69687471edf15d4d26552bb9adc2c155a9666a
Arryon Tijsma <arryon@gmail.com>
2019-02-12 08:53:41 +0100

    #35 Add equality operator to the Simhash class to compare by value
    Fix PEP-8 import order in unittests

--- tests/test_simhash.py
+++ tests/test_simhash.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from unittest import main, TestCase
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from simhash import Simhash, SimhashIndex
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 class TestSimhash(TestCase):
 
@@ -80,6 +81,16 @@ class TestSimhash(TestCase):
         # different results
         self.assertNotEqual(Simhash(dict_features).value,
                             Simhash(data[0]).value)
+
+    def test_equality_comparison(self):
+        a = Simhash('My name is John')
+        b = Simhash('My name is John')
+        c = Simhash('My name actually is Jane')
+
+        with self.subTest('A should equal B'):
+            self.assertEqual(a, b)
+        with self.subTest('A should not equal C'):
+            self.assertNotEqual(a, c)
 
 
 class TestSimhashIndex(TestCase):
