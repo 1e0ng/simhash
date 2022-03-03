78f3b8d5b810d93443269b6e227cae7b95e67316
Leon <i@leons.im>
2022-03-03 09:24:09 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'simhash',
-    version = '2.1.1',
+    version = '2.1.2',
     keywords = ('simhash'),
     description = 'A Python implementation of Simhash Algorithm',
     license = 'MIT License',
